c5e7dbb7501c9d3cd91325790c96d2dcf2ad4345
KK66 <>
2022-02-23 01:43:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Welcome to Split my Pizza")
+print("How many slices of pizza would you like to have?")
+pizzaSlices = int(input())
+print("How many people are you sharing the pizza?")
+people = int(input())
+
+Slices = pizzaSlices // people
+
+RemainingSlices = pizzaSlices % people
+
+print (f"There are going to be {Slices} Pineapple Pizza slices per person")
+print (f"There are {RemainingSlices} remaining Pieapple Pizza slices")
